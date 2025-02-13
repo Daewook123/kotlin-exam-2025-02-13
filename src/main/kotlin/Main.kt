1243cf24dddb9cfa74fc928878f98e063a83c3e9
Daewook123 <cdo5080@gmail.com>
2025-02-13 14:42:12 +0900

    명언앱 1단계 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package com.ll
 
 fun main() {
-    println("Hello World!")
+    while(true){
+        println("== 명언 앱 ==")
+        println("명령")
+        val command = readlnOrNull() ?: ""
+
+        when (command) {
+            "종료" ->{
+                break;
+            }
+        }
+//        // 사용자로부터 명언 입력 받기
+//        print("명언을 입력하세요: ")
+//
+//
+//        // 사용자로부터 저자 입력 받기
+//        print("저자를 입력하세요: ")
+//        val author = readLine() ?: ""
+//
+//        // Quote 클래스의 객체 생성
+//        val quote = Quote(text, author)
+//        println("명언: ${quote.text} - ${quote.author}")
+    }
 }
---
+++ src/main/kotlin/Quote.kt
@@ -0,0 +1,5 @@
+package com.ll
+
+class Quote(var text: String, var author: String) {
+
+}
