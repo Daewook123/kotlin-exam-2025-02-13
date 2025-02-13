dbacba2eb711b5d47b6f03b1a436813798200efa
Daewook123 <cdo5080@gmail.com>
2025-02-13 15:03:54 +0900

    명언앱 2단계 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,27 @@
 package com.ll
 
 fun main() {
+
+    val quoteList = mutableListOf<Quote>()
     while(true){
         println("== 명언 앱 ==")
-        println("명령")
+        print("명령 : ")
         val command = readlnOrNull() ?: ""
 
         when (command) {
+            "등록" ->{
+                print("명언을 입력하세요 : ")
+                val text = readlnOrNull() ?: ""
+
+                print("저자를 입력하세요 : ")
+                val author = readlnOrNull() ?: ""
+
+                quoteList.add(Quote(text, author));
+                println("${quoteList.size}번째 명언이 등록되었습니다.")
+            }
             "종료" ->{
                 break;
             }
         }
-//        // 사용자로부터 명언 입력 받기
-//        print("명언을 입력하세요: ")
-//
-//
-//        // 사용자로부터 저자 입력 받기
-//        print("저자를 입력하세요: ")
-//        val author = readLine() ?: ""
-//
-//        // Quote 클래스의 객체 생성
-//        val quote = Quote(text, author)
-//        println("명언: ${quote.text} - ${quote.author}")
     }
 }
